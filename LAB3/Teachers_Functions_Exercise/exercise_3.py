aac3707d647248615a391df8458fb42e88eb5886
NikiNyt <niki_net_yt@mail.ru>
2024-02-05 18:41:06 +0600

    mistake corrected

--- LAB3/Teachers_Functions_Exercise/exercise_3.py
+++ LAB3/Teachers_Functions_Exercise/exercise_3.py
@@ -10,4 +10,4 @@ num1 = int(input())
 num2 = int(input())
 num3 = int(input())
 
-print("maximum og three numbers is:", get_max(num1, num2, num3))
+print("maximum of three numbers is:", get_max(num1, num2, num3))
